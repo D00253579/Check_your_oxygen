586e4269b652815202eb902a172b00530bffaf2c
D00253579 <D00253579@student.dkit.ie>
2025-01-10 15:09:41 +0000

    :sparkles: Completed user login
    Completed user login so that a user can login if their email and
    password is correct, if not then redirects to login again

--- mongoDB.py
+++ mongoDB.py
@@ -2,6 +2,7 @@ from flask_mongoengine import MongoEngine
 from mongoengine import Document, StringField, IntField
 import base64
 import datetime
+import bcrypt
 
 # from .__init__ import db
 from __init__ import db
@@ -15,11 +16,28 @@ class Users(db.Document):
     user_password = db.StringField()
 
 
+def hashPassword(password):
+    hashedPassword = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashedPassword.decode()
+
+
 def add_user(firstName, lastName, email, password):
     if firstName and lastName and email and password is not False:
+        hashedPassword = hashPassword(password)
         Users(
             first_name=firstName,
             last_name=lastName,
             user_email=email,
-            user_password=password,
+            user_password=hashedPassword,
         ).save()
+
+
+def check_users(email, password):
+    doesUserExist = False
+    for user in Users.objects:
+        if user.user_email.lower() == email.lower():
+            if bcrypt.checkpw(password.encode(), user.user_password.encode()):
+                doesUserExist = True
+                break
+
+    return doesUserExist
